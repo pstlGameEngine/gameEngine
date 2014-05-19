c62ca1fb8f1aca045ffa14be1bb62b70d49f97d0
ghiles <ghiles@ghiles-TravelMate-5744.(none)>
2014-05-19 10:06:23 +0200

    suite pacman (pas fini)

--- examples/pacman/pacman.ml
+++ examples/pacman/pacman.ml
@@ -13,6 +13,7 @@
     val drawState : Mjwindow.t -> state -> unit
     val drawGhosts : Mjwindow.t -> state -> unit
 end*)
+open Keyboard
 
 module Pacman =
  struct 
@@ -44,7 +45,7 @@ module Pacman =
    score:int
   }
 
-  let isInField s c = (c.x>=0)&&(c.y>=0)&&(c.x<=s.sizeX)&&(c.y<=s.sizeY)
+  let isInField s c = (c.x>=0)&&(c.y>=0)&&(c.x<s.sizeX)&&(c.y<s.sizeY)
 
   let getCase s x y =
    List.nth s.field (y*s.sizeX +x)
@@ -56,6 +57,8 @@ module Pacman =
 
   let getIndex s c = c.y*s.sizeX + c.x
 
+
+(* On considère i1<i2*)
   let replace li index1 newElt1 index2 newElt2 =
    let rec replaceAux l i1 n1 i2 n2 nb =
     match l with    
@@ -84,7 +87,12 @@ module Pacman =
     | _ -> isInField s c
 
   let isLegalMove s c d =
-   isLegalPos s (getCaseFromDirection s c d)
+   (match d with 
+    Right -> c.x < s.sizeX-1
+    | Left -> c.x > 0
+    | Up -> c.y < s.sizeY-1
+    | Down -> c.y > 0)
+   && (isLegalPos s (getCaseFromDirection s c d))   
 
   let eatContent s c =
    match c.t with 
@@ -97,11 +105,11 @@ module Pacman =
                 
   let movePacman s d =
    let c0 = getCasePacman s in
-   let c1 = getCaseFromDirection s c0 d in 
-   if (isLegalPos s c1) then
+   if (isLegalMove s c0 d) then
     begin
+     let c1 = getCaseFromDirection s c0 d in 
      eatContent s c1;
-     {s with field = (updateCase s (getIndex s c0) (getIndex s c1) 
+     {s with field = (updateCase s (getIndex s c0) (getIndex s c1)
                                    {c0 with t=Case Empty} 
     				   {c1 with t=Case Pacman});
              pac = getIndex s c1
@@ -111,34 +119,41 @@ module Pacman =
    
   let moveGhost s g =
    Random.self_init();
-   let ld = List.map (fun e-> getCaseFromDirection s g e) [Up;Down;Right;Left]
-    in let lf = List.filter (fun e-> isLegalPos s e) ld 
-     in let random = Random.int (List.length lf) 
-      in let c1 = List.nth lf random
+   let liste_dir = [Up;Down;Right;Left]
+    in let liste_dir_legales = List.filter (fun e-> isLegalMove s g e) liste_dir 
+     in let random = Random.int (List.length liste_dir_legales) 
+      in let c1 = getCaseFromDirection s g (List.nth liste_dir_legales random)
        in match g.t, c1.t with 
-           (Case GhostEmpty), Case Empty -> updateCase s (getIndex s g) (getIndex s c1) 
+           (Case GhostEmpty), Case Empty -> {s with field = (updateCase s (getIndex s g) (getIndex s c1) 
                                    {g with t=Case Empty} 
-    				   {c1 with t=Case GhostEmpty}
-           | Case GhostFood, Case Empty -> updateCase s (getIndex s g) (getIndex s c1) 
+    				   {c1 with t=Case GhostEmpty}) ; ghosts=[getIndex s c1]}
+           | Case GhostFood, Case Empty -> {s with field = (updateCase s (getIndex s g) (getIndex s c1) 
                                    {g with t=Case Food} 
-    				   {c1 with t=Case GhostEmpty}
-           | Case GhostEmpty, Case Food -> updateCase s (getIndex s g) (getIndex s c1) 
+    				   {c1 with t=Case GhostEmpty}) ; ghosts=[getIndex s c1]}
+           | Case GhostEmpty, Case Food -> {s with field = (updateCase s (getIndex s g) (getIndex s c1) 
                                    {g with t=Case Empty} 
-    				   {c1 with t=Case GhostFood}
-           | Case GhostFood, Case Food -> updateCase s (getIndex s g) (getIndex s c1) 
+    				   {c1 with t=Case GhostFood}) ; ghosts=[getIndex s c1]}
+           | Case GhostFood, Case Food -> 
+                                    print_string "lalala";print_newline ();
+				   {s with field = (updateCase s (getIndex s g) (getIndex s c1) 
                                    {g with t=Case Food} 
-    				   {c1 with t=Case GhostFood}
+    				   {c1 with t=Case GhostFood}) ; ghosts=[getIndex s c1]}
+           | _,_ ->  print_string "lili";print_newline ();s
            
   
    
-(*
  let moveAllGhost s = 
-   let newG = List.map (fun e -> moveGhost s e) s.ghost in
-    *) 
+  let rec aux li s =
+   match li with 
+    [] -> s
+    | h::q -> aux q (moveGhost s (List.nth s.field h))  
+
+  in aux s.ghosts s
+
 
   let isOver s = false
 
-  let tick s = s
+  let tick s = moveAllGhost s
 
   let print_state s = ()
 
@@ -147,11 +162,23 @@ module Pacman =
         else if s1.score < s2.score then -1
         else 0
 
-  let changeStateWithEvent s fen = s
+  let changeStateWithEvent s fen = 
+        begin
+	  if Keyboard.is_key_pressed KeyCode.Left then
+            movePacman s Left
+          else if Keyboard.is_key_pressed KeyCode.Right then
+            movePacman s Right
+          else if Keyboard.is_key_pressed KeyCode.Up then
+            movePacman s Down
+          else if Keyboard.is_key_pressed KeyCode.Down then
+            movePacman s Up
+          else
+           s
+        end
 
-  let recordCondition s = false
+  let recordCondition s = true
 
-  let backInTimeCondition s = false
+  let backInTimeCondition s = Keyboard.is_key_pressed KeyCode.B
 
   let drawGhosts fen s = ()
 
@@ -165,21 +192,41 @@ module Pacman =
   let drawPacman fen s = 
    let c = getCasePacman s in
     let couple = scale s fen c.x c.y in
-    Mjwindow.fillCircle fen (fst couple) (snd couple) 10. Color.yellow
+    Mjwindow.fillCircle fen ((fst couple)+.50.) ((snd couple)+.50.) 20. Color.yellow
+
+  let drawGhost fen s x y =
+    let couple = scale s fen x y in
+    Mjwindow.fillCircle fen ((fst couple)+.50.) ((snd couple)+.50.) 10. Color.blue
+
+  let drawWall fen s x y =
+    let couple = scale s fen x y in
+    Mjwindow.fillRect fen (fst couple) (snd couple) 100. 100. Color.red
+  
+  let drawFood fen s x y = 
+    let couple = scale s fen x y in
+    Mjwindow.fillCircle fen ((fst couple)+.50.) ((snd couple)+.50.) 5. Color.white
+
+  let drawCase fen c s =
+   match c.t with 
+     Wall -> drawWall fen s c.x c.y
+     | Case GhostFood | Case GhostEmpty ->  drawGhost fen s c.x c.y
+     | Case Food -> drawFood fen s c.x c.y
+     | Case Pacman -> drawPacman fen s
+     | _ -> ()
 
   let drawState fen s = 
-    drawPacman fen s
+   List.iter (fun e-> drawCase fen e s) s.field  
               
    let init fen = 
     let field = [ 
 	{y=0;x=0;t=Wall};	 {y=0;x=1;t=Wall};	   {y=0;x=2;t=Case Food};  {y=0;x=3; t=Case Food};      {y=0;x=4;t=Case Food}; 
-        {y=1;x=0;t=Case Food};   {y=1;x=1;t=Wall};	   {y=1;x=2;t=Case Food};  {y=1;x=3;t=Case Food};       {y=1;x=4;t=Case Food}; 
-        {y=2;x=0;t=Case Food};   {y=2;x=1;t=Wall};	   {y=2;x=2;t=Case Food};  {y=2;x=3;t=Case Food};       {y=2;x=4;t=Case Bonus}; 
-        {y=3;x=0;t=Wall};        {y=3;x=1;t=Case Pacman};  {y=3;x=2;t=Case Food};  {y=3;x=3;t=Case GhostFood};  {y=3;x=4;t=Case GhostFood}; 
-        {y=4;x=0;t=Case Bonus};  {y=4;x=1;t=Case Food};    {y=4;x=2;t=Case Food};  {y=4;x=3;t=Case Food};       {y=4;x=4;t=Case Bonus}
+        {y=1;x=0;t=Case Food};   {y=1;x=1;t=Wall};	   {y=1;x=2;t=Wall};  {y=1;x=3;t=Case Food};       {y=1;x=4;t=Case Food}; 
+        {y=2;x=0;t=Case Food};   {y=2;x=1;t=Case Food};	   {y=2;x=2;t=Case Food};  {y=2;x=3;t=Case Food};       {y=2;x=4;t=Case Food}; 
+        {y=3;x=0;t=Wall};        {y=3;x=1;t=Case Pacman};  {y=3;x=2;t=Case Food};  {y=3;x=3;t=Case Food};  {y=3;x=4;t=Case GhostFood}; 
+        {y=4;x=0;t=Case Food};  {y=4;x=1;t=Case Food};    {y=4;x=2;t=Case Food};  {y=4;x=3;t=Case Food};       {y=4;x=4;t=Case Food}
     ] in
       let pac      = 16 
-      and ghosts   = [18;19] 
+      and ghosts   = [19] 
       and sizeX    = 5 
       and sizeY    = 5 
       and time_bar = {current=0;max=50}
@@ -194,3 +241,20 @@ module Pacman =
 	score=score
       }
  end  
+
+
+ module Pac = Timebis.TIMELINE(Pacman);;
+
+let _ =
+ let fen = Mjwindow.create_window 500. 500. "Pacman" 3 Color.black in
+ Pac.play fen Pac.DrawThenEvent 2
+ 
+
+
+
+
+
+
+
+
+
